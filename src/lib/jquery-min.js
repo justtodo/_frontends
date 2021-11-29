c9e577d90c2c946d63748d3908a05eb96b151eb9
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2021-11-29 14:48:17 +0100

    btn burger noFonction but Dark mode is okay

--- src/lib/jquery-min.js
+++ src/lib/jquery-min.js
@@ -1,38 +1,12 @@
-// // When user clicks anywhere outside of the Modal, close Modal
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
-// modal.addEventListener("click", () => {
-//   modal.classList.toggle("active");
-// });
-
-// const toggleButton = document.getElementById("js-burger");
-// const sideBar = document.getElementById("side-bar");
-
-// toggleButton.addEventListener("click", show);
-
-// function show() {
-//   sideBar.classList.toggle("active");
-// }
-
-// // REMOVE SIDEBAR IF CLICK ON THE MAIN CONTENT
-// const content = document.querySelector(".content");
-
-// content.addEventListener("click", () => {
-//   sideBar.classList.remove("active");
-// });
-
 // jquerie make a burger
 
 $("#js-burger").click(function () {
-  $("#header").slideToggle(); //fait disparaître le contenu en params
+  $(this).toggleClass(".modal-header");
 });
 
-$("#moon").click(function () {
-  $("*").css("color", "#ccc").css("background", "#121c4299").fadeIn(200);
+$(".switch").click(function () {
+  $("body").toggle().css("color", "#fff").css("background", "#121c4299");
+  //
 });
 // $("#sun").click(function () {
 //   $("*").css("color", "#000").css("background", "#f3f9fb").fadeOut(200);
